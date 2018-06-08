628b023872e42bc9c915de5277660086b15ba585
Shea Belsky <shb95@cornell.edu>
2018-06-08 07:13:01 -0700

    Runs better, fixing publicPath

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,7 +17,7 @@ module.exports = {
     entry: `${__dirname}/src/index.js`,
     output: {
         path: `${__dirname}/docs`,
-        publicPath: "/",
+        publicPath: "/georgeHascup",
         filename: "js/bundle.js",
     },
     devServer: {
