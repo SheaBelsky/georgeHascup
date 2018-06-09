a8fe89f26d07065e4087cb2dd92b15fbc8d3463e
Shea Belsky <shb95@cornell.edu>
2018-06-09 08:50:55 -0700

    Still having problems...

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -11,7 +11,7 @@ module.exports = {
     entry: `${__dirname}/src/index.js`,
     output: {
         path: `${__dirname}/docs`,
-        publicPath: "georgeHascup",
+        publicPath: "/georgeHascup",
         filename: "js/bundle.js",
     },
     devServer: {
