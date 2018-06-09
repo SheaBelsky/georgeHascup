6f212193b6f399dabc2c5e5ac8e144a6a1d091b9
Shea Belsky <shb95@cornell.edu>
2018-06-09 08:44:19 -0700

    think I got it

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,7 +17,7 @@ module.exports = {
     entry: `${__dirname}/src/index.js`,
     output: {
         path: `${__dirname}/docs`,
-        publicPath: "/georgeHascup",
+        publicPath: "georgeHascup",
         filename: "js/bundle.js",
     },
     devServer: {
