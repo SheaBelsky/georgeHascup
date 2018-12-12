4d351b5ec03df2e0d59622fbc7601afe017558b6
Shea Belsky <shb95@cornell.edu>
2018-12-12 15:35:45 -0500

    one last time

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@ const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const path                    = require("path");
 const webpack                 = require("webpack");
 
-const SITE_URL = "https://sheabelsky.github.io/georgeHascup/";
+const SITE_URL = "https://www.georgehascup.com/";
 
 const plugins = [
     new ExtractTextPlugin({
